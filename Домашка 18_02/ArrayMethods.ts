900c544d0b5d5387b2baabfe9723bd214a3d1f71
Alex <defansor125@gmail.com>
2025-02-26 15:38:01 +0100

    Переделал на дженерики

--- Домашка 18_02/ArrayMethods.ts
+++ Домашка 18_02/ArrayMethods.ts
@@ -1,14 +1,15 @@
 // реализовать push, map, join, fill, includes, indexOF
 
-function myMap(
-  array: number[],
-  fn: (item: number, index: number) => number
-): number[] {
-  const newArray: number[] = [];
+// T1 - тип массива который на входе
+// T2 - тип массива который на выходе
+function myMap<T1, T2>(array: T1[], fn: (item: T1, index: number) => T2): T2[] {
+  const newArray: T2[] = [];
+
   for (let i = 0; i < array.length; i++) {
     // newArray.push(fn(array[i], i));
     myPush(newArray, fn(array[i], i));
   }
+
   return newArray;
 }
 
@@ -27,45 +28,40 @@ function myMap(
 
 const myArray: number[] = [1, 2, 3, 4, 5, 6, 7];
 
-console.log(
-  myArray.map((item) => {
-    return item * 2;
-  })
-);
+// map = transform
+// myArray.transform((item) => item * 2)
+console.log(myArray.map((item) => item * 2));
 
-console.log(
-  myMap(myArray, (item) => {
-    return item * 2;
-  })
-);
+console.log(myMap(myArray, (item) => item * 2));
+console.log(myMap([""], (item) => "".length));
 
 //push
 
-function myPush(array: number[], item: number): number[] {
+function myPush<T>(array: T[], item: T): number {
   array[array.length] = item; // не понимаю всё равно как это работает.
-  return array;
+  return array.length;
 }
 
-console.log(myPush(myArray, 15));
+myPush(myArray, 15);
+console.log(myArray);
+[].push;
 
-function myJoin(array: string[], glue: string): void {
+function myJoin<T>(array: T[], glue: string): string {
   let result: string = "";
-  for (let i = 0; i < array.length; i++) {
-    const str = array[i];
-    if (i != array.length - 1) {
-      result = result + str + glue;
-    } else {
-      result = result + str;
-    }
+
+  for (let i = 0; i < array.length - 1; i++) {
+    // result = result + array[i] + glue;
+    result += array[i] + glue;
   }
-  console.log(result);
+
+  return result + array[array.length - 1];
 }
 
-myJoin(["hello", "privet", "okay"], ";");
+console.log(myJoin(["hello", "privet", "okay"], ";"));
 
 // const myArray: number[] = [1, 2, 3, 4, 5, 6, 7];
 
-function myFill(array: any[], value: any, start: number, end: number): any[] {
+function myFill<T>(array: T[], value: T, start: number, end: number): T[] {
   for (let i = start; i <= end; i++) {
     array[i] = value;
   }
@@ -77,9 +73,9 @@ console.log(myFill(myArray, 0, 2, 6));
 console.log(myArray.fill(0, 2, 6));
 
 const myArray1: number[] = [1, 2, 3, 4, 5, 6, 7];
-function myIndexOf(array: any[], item: any, from: number) {
+
+function myIndexOf<T>(array: T[], item: T, from: number): number {
   for (let i = from; i < array.length; i++) {
-    console.log("🚀 ~ myIndexOf ~ array[i]:", array[i]);
     if (array[i] == item) return i;
   }
   return -1;
@@ -87,10 +83,26 @@ function myIndexOf(array: any[], item: any, from: number) {
 
 console.log(myIndexOf(myArray1, 3, 0));
 
-function myIncludes(array: any[], item: any, from: number) {
+function printMultiplicationTable1(): void {
+  for (let i = 1; i < 10; i++) {
+    const result: string[] = [];
+
+    for (let j = 1; j < 10; j++) {
+      result.push(`${j} * ${i} = ${j * i}`.padEnd(10, " "));
+    }
+
+    console.log(myJoin(result, "  "));
+  }
+}
+
+printMultiplicationTable1();
+
+function myIncludes<T>(array: T[], item: T, from: number): boolean {
   for (let i = from; i < array.length; i++) {
     console.log("🚀 ~ myIndexOf ~ array[i]:", array[i]);
     if (array[i] == item) return true;
   }
   return false;
 }
+
+console.log(myIncludes([0, 1, 2, 3, 4, 5, 6], 5, 3));
